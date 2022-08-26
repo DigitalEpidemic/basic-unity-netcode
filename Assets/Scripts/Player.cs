0378b61e13e5fd3da0f6c940166c6415e79974dc
Jeffrey Polasz <jeff_polasz@hotmail.com>
2022-08-26 02:35:16 -0400

    Add ability to rotate player and sync over network

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Player : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> position = new();
-    
+    public NetworkVariable<Vector3> positionNetworkVariable = new();
+
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private Transform _spawnPointTransform;
-    
+
+    private int rotateSpeed = 200;
+
     private GameObject _projectileGameObject;
     private NetworkObject _projectileNetworkObject;
+
 
     public override void OnNetworkSpawn()
     {
@@ -20,16 +24,27 @@ public class Player : NetworkBehaviour
         }
     }
     
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position = position.Value;
+        transform.position = positionNetworkVariable.Value;
+
+        RotatePlayer();
     }
-    
+
+    private void RotatePlayer()
+    {
+        if (IsClient && IsOwner)
+        {
+            Vector3 inputRotation = new Vector3(0, Input.GetAxis("Horizontal") * Time.fixedDeltaTime * rotateSpeed, 0);
+            transform.Rotate(inputRotation, Space.World);
+        }
+    }
+
     public void Move()
     {
         if (NetworkManager.Singleton.IsServer)
         {
-            position.Value = GetRandomPositionOnPlane();
+            positionNetworkVariable.Value = GetRandomPositionOnPlane();
         }
         else
         {
@@ -40,7 +55,7 @@ public class Player : NetworkBehaviour
     [ServerRpc]
     private void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
     {
-        position.Value = GetRandomPositionOnPlane();
+        positionNetworkVariable.Value = GetRandomPositionOnPlane();
     }
 
     public void Fire()
