6cb7975eabbc1d5bdf7eb39cae938ad95c4c1bcb
Jeffrey Polasz <jeff_polasz@hotmail.com>
2022-08-26 02:53:00 -0400

    Destroy projectiles on server after 5 seconds

--- Assets/Scripts/ServerProjectile.cs
+++ Assets/Scripts/ServerProjectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,8 +7,17 @@ using UnityEngine;
 public class ServerProjectile : NetworkBehaviour
 {
     [SerializeField] private int speed = 15;
+    
     private void Update()
     {
         transform.position += -transform.forward * speed * Time.deltaTime;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            Destroy(gameObject, 5f);
+        }
     }
 }
